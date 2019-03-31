1c948114654e103e9262e8c99cb86b2796664a0d
Hal Perry Cardenas <hal96.perry@gmail.com>
2019-03-31 12:27:34 -0600

    start of implementation

---
+++ semcube.py
@@ -0,0 +1,73 @@
+class semcube:
+  def__init__(self):
+    self.cube = {'int':{
+                        '+':{
+                            'int' : 'int',
+                            'flo' : 'flo',
+                            },
+                        '-':{
+                            'int' : 'int',
+                            'flo' : 'flo'
+                            },
+                        '*':{
+                            'int' : 'int',
+                            'flo' : 'flo'
+                        },
+                        '/':{
+                            'int' : 'int',
+                            'flo' : 'flo'
+                        },
+                        '=':{
+                            'int' : 'int',
+                            'flo' : 'error type mysmatch'
+                        },
+                        '==':{
+                            'int' : 'boo'
+                            'flo' : 'boo'
+                        },
+                        '<':{
+                            'int' : 'boo',
+                            'flo' : 'boo',
+                            'cha' : 'error type mismatch',
+                            'boo' : 'error type mismatch',
+                            'str' : 'error type mismatch',
+                            'void' : 'error type mismatch'
+
+                        },
+                        '>':{
+                            'int' : 'boo',
+                            'flo' : 'boo',
+                            'cha' : 'error type mismatch'
+                            'str' : 'error type mismatch'
+                            'boo' : 'error type mismatch'
+                            'void' : 'error type mismatch'
+                        }}
+                        '<=':{
+                            'int' : 'boo',
+                            'flo' : 'boo',
+                            'cha' : 'error type mismatch',
+                            'boo' : 'error type mismatch',
+                            'str' : 'error type mismatch',
+                            'void' : 'error type mismatch'
+
+                        },
+                        '>=':{
+                            'int' : 'boo',
+                            'flo' : 'boo',
+                            'cha' : 'error type mismatch'
+                            'str' : 'error type mismatch'
+                            'boo' : 'error type mismatch'
+                            'void' : 'error type mismatch'
+                        },
+                        '!=':{
+                            'int' : 'boo',
+                            'flo' : 'boo',
+                            'cha' : 'error type mismatch'
+                            'str' : 'error type mismatch'
+                            'boo' : 'error type mismatch'
+                            'void' : 'error type mismatch'
+
+                        }}
+
+                            
+                } 
