d345bade0a934efbe619ce787a1b6485712fad7f
Luis Carlos Figueroa Rodríguez <lc.figueroar26@gmail.com>
2019-05-04 17:29:52 -0500

    VM assigns values to local vars and uses them in arithmetic operations

--- virtual_machine.py
+++ virtual_machine.py
@@ -1,164 +1,70 @@
 from parser import quadruples, memory
-from virtual_memory import Memory
+from virtual_memory import BigMemory, Memory
 import operator
 
 memoryStack = []
-actualMemory = Memory(100000, 110000)
-Memory(300000, 310000)
-globalMemory = Memory(200000, 210000)
-constantMemory = Memory(300000, None)
+vm_memory = BigMemory()
+# actualMemory = Memory(100000, 110000)
+# Memory(300000, 310000)
+# globalMemory = Memory(200000, 210000)
+# constantMemory = Memory(300000, None)
 
 arithmetic_operations = { '+' : operator.add, '-' : operator.sub, '*' : operator.mul, '/' : operator.truediv }
 
 def execute_quadruple(quad, ip):
   global memoryStack
-  global actualMemory
+  global vm_memory
   instruction = quad.operator
   pointer = ip
   if instruction == 'GoTo':
     create_local_memory()
     return quad.result
   elif instruction == 'ERA':
-    memoryStack.append(actualMemory)
+    memoryStack.append(vm_memory.locals)
     create_local_memory()
   elif instruction == 'GoSub':
-    actualMemory = memoryStack.pop()
+    vm_memory.set_local(memoryStack.pop())
   elif instruction == '+' or instruction == '-' or instruction == '*' or instruction == '/':
-    execute_aritmetic_operation(instruction, get_quad_value(quad.op1), get_quad_value(quad.op2), quad.result)
+    execute_aritmetic_operation(instruction, quad.op1, quad.op2, quad.result)
   elif instruction == 'Writing':
     output_msg(quad.result)
+  elif instruction == '=':
+    execute_assign(vm_memory.real_memory(quad.op1).get_value(quad.op1), quad.result)
   pointer += 1
   return pointer
 
 # Executes aritmetic operation according to operator
 def execute_aritmetic_operation(op, left, right, res_direction):
-  result =  arithmetic_operations[op](left, right)
-  insert_into_corresponding_direction(res_direction, result)
+  global vm_memory
+  result =  arithmetic_operations[op](vm_memory.real_memory(left).get_value(left), vm_memory.real_memory(right).get_value(right))
+  vm_memory.real_memory(res_direction).push(result)
+
+# Executes assign operation
+def execute_assign(left, res_direction):
+  global vm_memory
+  variable_in_memory(res_direction)
+  vm_memory.real_memory(res_direction).set_value(res_direction, left)
 
 # Creates local Memory for function
 def create_local_memory():
-  global actualMemory
-  actualMemory = Memory(100000, 110000)
+  global vm_memory
+  vm_memory.set_local(Memory(100000, 110000))
 
 # Print function for Writing quad instruction
-def output_msg(message):
-  print(get_quad_value(message))
+def output_msg(direction):
+  global vm_memory
+  print(vm_memory.real_memory(direction).get_value(direction))
 
-# Inserts value into corresponding memory direction
-def insert_into_corresponding_direction(direction, value):
-  global actualMemory
-  global constantMemory
-  global globalMemory
-  if 100000 <= direction and direction <= 101999:   # LOCALS
-    actualMemory.variables.int.push(value)
-  elif 102000 <= direction and direction <= 103999:
-    actualMemory.variables.flo.push(value)
-  elif 104000 <= direction and direction <= 105999:
-    actualMemory.variables.str.push(value)
-  elif 106000 <= direction and direction <= 107999:
-    actualMemory.variables.cha.push(value)
-  elif 108000 <= direction and direction <= 109999:
-    actualMemory.variables.boo.push(value)
-  elif 110000 <= direction and direction <= 111999:
-    actualMemory.temporals.int.push(value)
-  elif 112000 <= direction and direction <= 113999:
-    actualMemory.temporals.flo.push(value)
-  elif 114000 <= direction and direction <= 115999:
-    actualMemory.temporals.str.push(value)
-  elif 116000 <= direction and direction <= 117999:
-    actualMemory.temporals.cha.push(value)
-  elif 118000 <= direction and direction <= 119999:
-    actualMemory.temporals.boo.push(value)
-  elif 200000 <= direction and direction <= 201999: # GLOBAL
-    globalMemory.variables.int.push(value)
-  elif 202000 <= direction and direction <= 203999:
-    globalMemory.variables.flo.push(value)
-  elif 204000 <= direction and direction <= 205999:
-    globalMemory.variables.str.push(value)
-  elif 206000 <= direction and direction <= 207999:
-    globalMemory.variables.cha.push(value)
-  elif 208000 <= direction and direction <= 209999:
-    globalMemory.variables.boo.push(value)
-  elif 210000 <= direction and direction <= 211999:
-    globalMemory.temporals.int.push(value)
-  elif 212000 <= direction and direction <= 213999:
-    globalMemory.temporals.flo.push(value)
-  elif 214000 <= direction and direction <= 215999:
-    globalMemory.temporals.str.push(value)
-  elif 216000 <= direction and direction <= 217999:
-    globalMemory.temporals.cha.push(value)
-  elif 218000 <= direction and direction <= 219999:
-    globalMemory.temporals.boo.push(value)
-  elif 300000 <= direction and direction <= 301999: # CONSTANTS
-    constantMemory.variables.int.push(value)
-  elif 302000 <= direction and direction <= 303999:
-    constantMemory.variables.flo.push(value)
-  elif 304000 <= direction and direction <= 305999:
-    constantMemory.variables.str.push(value)
-  elif 306000 <= direction and direction <= 307999:
-    constantMemory.variables.cha.push(value)
-  elif 308000 <= direction and direction <= 309999:
-    constantMemory.variables.boo.push(value)
-  
-def get_quad_value(direction):
-  global actualMemory
-  global constantMemory
-  global globalMemory
-  if 100000 <= direction and direction <= 101999:           # LOCALS
-    return actualMemory.variables.int.get_value(direction)
-  elif 102000 <= direction and direction <= 103999:
-    return actualMemory.variables.flo.get_value(direction)
-  elif 104000 <= direction and direction <= 105999:
-    return actualMemory.variables.str.get_value(direction)
-  elif 106000 <= direction and direction <= 107999:
-    return actualMemory.variables.cha.get_value(direction)
-  elif 108000 <= direction and direction <= 109999:
-    return actualMemory.variables.boo.get_value(direction)
-  elif 110000 <= direction and direction <= 111999:
-    return actualMemory.temporals.int.get_value(direction)
-  elif 112000 <= direction and direction <= 113999:
-    return actualMemory.temporals.flo.get_value(direction)
-  elif 114000 <= direction and direction <= 115999:
-    return actualMemory.temporals.str.get_value(direction)
-  elif 116000 <= direction and direction <= 117999:
-    return actualMemory.temporals.cha.get_value(direction)
-  elif 118000 <= direction and direction <= 119999:
-    return actualMemory.temporals.boo.get_value(direction)
-  elif 200000 <= direction and direction <= 201999:         # GLOBAL
-    return globalMemory.variables.int.get_value(direction)
-  elif 202000 <= direction and direction <= 203999:
-    return globalMemory.variables.flo.get_value(direction)
-  elif 204000 <= direction and direction <= 205999:
-    return globalMemory.variables.str.get_value(direction)
-  elif 206000 <= direction and direction <= 207999:
-    return globalMemory.variables.cha.get_value(direction)
-  elif 208000 <= direction and direction <= 209999:
-    return globalMemory.variables.boo.get_value(direction)
-  elif 210000 <= direction and direction <= 211999:
-    return globalMemory.temporals.int.get_value(direction)
-  elif 212000 <= direction and direction <= 213999:
-    return globalMemory.temporals.flo.get_value(direction)
-  elif 214000 <= direction and direction <= 215999:
-    return globalMemory.temporals.str.get_value(direction)
-  elif 216000 <= direction and direction <= 217999:
-    return globalMemory.temporals.cha.get_value(direction)
-  elif 218000 <= direction and direction <= 219999:
-    return globalMemory.temporals.boo.get_value(direction)
-  elif 300000 <= direction and direction <= 301999:         # CONSTANTS
-    return constantMemory.variables.int.get_value(direction)
-  elif 302000 <= direction and direction <= 303999:
-    return constantMemory.variables.flo.get_value(direction)
-  elif 304000 <= direction and direction <= 305999:
-    return constantMemory.variables.str.get_value(direction)
-  elif 306000 <= direction and direction <= 307999:
-    return constantMemory.variables.cha.get_value(direction)
-  elif 308000 <= direction and direction <= 309999:
-    return constantMemory.variables.boo.get_value(direction)
+# Check if variable direction exists in memory
+def variable_in_memory(direction):
+  global vm_memory
+  while(0 <= direction - (len(vm_memory.real_memory(direction).vars) + vm_memory.real_memory(direction).offset)):
+    vm_memory.real_memory(direction).push(None)
 
 pointer = 0
 if len(memory) > 0:
   for var in memory:
-    insert_into_corresponding_direction(var, memory[var])
+    vm_memory.real_memory(var).push(memory[var])
     
 while(pointer < len(quadruples)):
   pointer = execute_quadruple(quadruples[pointer], pointer)
--- virtual_memory.py
+++ virtual_memory.py
@@ -13,7 +13,7 @@ class VirtualMemory:
     self.vars.append(data)
 
   def set_value(self, dir, val):
-    self.vars[dir] = val
+    self.vars[dir - self.offset] = val
 
   def get_value(self, dir):
     return self.vars[dir - self.offset]
@@ -28,13 +28,58 @@ class Memories:
     self.cha = VirtualMemory(self.base_dir + CHA_CONS)
     self.boo = VirtualMemory(self.base_dir + BOO_CONS)
 
+  def get_type_by_direction(self, direction):
+    if(self.base_dir <= direction and direction < self.base_dir + 2000):
+      return self.int
+    elif(self.base_dir + FLO_CONS <= direction and direction < self.base_dir + 4000):
+      return self.flo
+    elif(self.base_dir + STR_CONS <= direction and direction < self.base_dir + 6000):
+      return self.str
+    elif(self.base_dir + CHA_CONS <= direction and direction < self.base_dir + 8000):
+      return self.cha
+    elif(self.base_dir + BOO_CONS <= direction and direction < self.base_dir + 10000):
+      return self.boo
+
 # Class that groups Memories according to Scope | Type relation (i.e. 200000 would set Memory for Global Variables and 110000 for Local Temporals)
 class Memory:
   def __init__(self, base_variables, base_temporals):
+    self.base_variables = base_variables
+    self.base_temporals = base_temporals
     self.variables = Memories(base_variables)
     if base_temporals == None:
       self.temporals = None
     else:
       self.temporals = Memories(base_temporals)
+
+  # Return Memories according to direction (variables or temporals)
+  def get_memory_by_direction(self, direction):
+    if(self.base_variables <= direction and (self.base_temporals == None or direction < self.base_temporals)):
+      return self.variables.get_type_by_direction(direction)
+    elif(self.base_temporals <= direction and direction < self.base_temporals + 10000):
+      return self.temporals.get_type_by_direction(direction)
+    else:
+      return None
+
+class BigMemory:
+  def __init__(self):
+    self.locals = None
+    self.globals = Memory(200000, 201000)
+    self.constants = Memory(300000, None)
+
+  def set_local(self, memory):
+    self.locals = memory
+
+  def get_real_memory_by_direction(self, direction):
+    if(300000 <= direction and direction < 400000):
+      return self.constants.get_memory_by_direction(direction)
+    elif(100000 <= direction and direction < 200000):
+      return self.locals.get_memory_by_direction(direction)
+    elif(200000 <= direction and direction < 300000):
+      return self.globals.get_memory_by_direction(direction)
+    else:
+      return None
+
+  def real_memory(self, direction):
+    return self.get_real_memory_by_direction(direction)
 
   
